f6621cf363579209b9fc6f80bf2cd20ed11005b7
Code <code@COCTL-41297.local>
2020-01-28 15:57:41 -0800

    Use angularProjects1-28 to make Apparel done right

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.scss']
 })
 export class CreateAccountComponent implements OnInit {
+  createForm: FormGroup;
+  link: string;
 
-  constructor() { }
+  constructor(private accountCreate: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.madeNewForm();
   }
 
+  madeNewForm() {
+    this.createForm = this.accountCreate.group({
+      fullName: [null, Validators.required],
+      userName: [null, Validators.required],
+      password: [null, Validators.required],
+      emailAddress: [null, Validators.required],
+      birth: [null, Validators.required],
+      gender: [null, Validators.required],
+      country: [null, Validators.required],
+      city: [null, Validators.required],
+      streetZip: [null, Validators.required],
+      secretQuestion: [null, Validators.required],
+      secretAnswer: [null, Validators.required]
+    });
+  }
 }
